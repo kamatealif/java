c5ea42847acccfade103c3fb095f5b089679c0c4
kamatealif <kamatealip1@gmail.com>
2025-01-12 20:39:39 +0530

    explored math library

--- 06_math/src/Circumference.java
+++ 06_math/src/Circumference.java
@@ -1,16 +1,49 @@
+import java.util.Objects;
 import java.util.Scanner;
 public class Circumference {
+    //formulas
+    // circumference = 2 * pi * radius
+    // area = pi * radius**2
+    // valume = (4.0/3.0) * pi * radius^2;
+    public static void menu(){
+        System.out.println("1. circumeference of traingle");
+        System.out.println("2. area of circle");
+        System.out.println("3. Valume");
+    }
+
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         double pi = Math.PI;
 
-        System.out.print("Enter radius: ");
-        double radius = scanner.nextDouble();
+        menu();
+        System.out.println("Enter the choice");
+       String choice = scanner.next();
 
-        double circumference = 2 * pi * radius;
-        
-        System.out.println("The circumference is: " + circumference);
+       if (Objects.equals(choice, "1")){
+           System.out.print("Enter radius: ");
+           double radius = scanner.nextDouble();
 
+           double circumference = 2 * pi * radius;
+
+           System.out.println("The circumference is: " + circumference);
+
+       } else if (Objects.equals(choice, "2")) {
+            System.out.print("Enter radius: ");
+            double radius = scanner.nextDouble();
+            double result;
+            result = pi * Math.pow(radius,2);
+
+            System.out.println("Area : " + result);
+       }else{
+            // valume = (4.0/3.0) * pi * radius^2;
+
+            System.out.print("Enter radius: ");
+            double radius = scanner.nextDouble();
+            double result;
+
+            result = (4.0/3.0) * pi * Math.pow(radius,2);
+            System.out.println("Valume: " + result);
+        }
         scanner.close();
     }
 }
