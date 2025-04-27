c956aa3c59014ff6965477cfadaacbbce439bdc6
kamatealif <kamatealip1@gmail.com>
2025-04-27 21:44:21 +0530

    Refactor discount logic in ticket pricing calculation

--- 08_nested_if_else/src/App.java
+++ 08_nested_if_else/src/App.java
@@ -1,16 +1,30 @@
 public class App{
     public static void main(String[] args){
 
-        boolean isStudent = true;
-        // boolean isSeniorCitizen;
+        boolean isStudent = false;
+        boolean isSenior = true;
         double ticketPrice = 9.99;
 
         if(isStudent){
-            System.out.println("You get a student discount of 10%");
-            ticketPrice = ticketPrice * 0.9;
+            if(isSenior){
+                System.out.println("You get a student and senior discount of 20%");
+                System.out.println("You get a student discount of 10%");
+                ticketPrice = ticketPrice * 0.7;
+            }
+            else{
+                System.out.println("You get a student discount of 10%");
+                ticketPrice = ticketPrice * 0.9;
+            }
         }
+            
         else{
-            ticketPrice = ticketPrice * 1.0;
+            if(isSenior){
+                System.out.println("You get a senior discount of 10%");
+                ticketPrice = ticketPrice * 0.9;
+            }
+            else{
+                System.out.println("You get no discount");
+            }
         }
 
         System.out.println("The Ticket price is: " + ticketPrice);
