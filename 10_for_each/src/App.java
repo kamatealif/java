b3c2c8761ab7fa6c2e80dc798ce4181806d67983
kamatealif <kamatealip1@gmail.com>
2025-04-29 21:55:21 +0530

    Refactor App.java to include findMaxLElement method for finding maximum in a list using for-each loop

--- 10_for_each/src/App.java
+++ 10_for_each/src/App.java
@@ -1,5 +1,7 @@
 import java.io.*;
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 public class App{
     public static void main(String[] args){
        // for each loop 
@@ -11,6 +13,11 @@ public class App{
         int[] arr ={112,1,2234,43892,9090,1283,189289};
         int maxNumber = findMax(arr);
         System.out.println("Max number is: " + maxNumber);
+
+
+        // find max number in a list using for each loop
+        System.out.println("find max number in a list using for each loop");
+        findMaxLElement();
     }
 
     public static void eacher(){
@@ -30,6 +37,27 @@ public class App{
         }
         return maximum;
 
+    }
+
+    // find the maximum element in a list using a for-each loop
+    public static  void findMaxLElement(){
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+        numbers.add(5);
+
+        // minimum integer value as max
+        int max = Integer.MIN_VALUE;
+        for( Integer i: numbers){
+            if(i > max){
+                max = i;
+            }
+        }
+
+        System.out.println("List of Integers:" + numbers);
+        System.out.println("Maximum element in the List : " + max);
     }
 
 }
