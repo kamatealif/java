4ebb45351446329672cc3d83ea5f83fef4016fae
kamatealif <kamatealip1@gmail.com>
2025-05-18 20:57:41 +0530

    sample Method

---
+++ 14_methods/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+
+    // method to print message
+    public  void printMessage(){
+        System.out.println("Hello worl!");
+    }
+    public static void main(String[] args) throws Exception {
+        // print message function call 
+        App obj = new App();
+        obj.printMessage();
+    }
+}
