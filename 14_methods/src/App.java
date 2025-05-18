e2f0a74282f0625dbd4c448275c116c112d4c243
kamatealif <kamatealip1@gmail.com>
2025-05-18 21:03:48 +0530

    class wise method to call the method

--- 14_methods/src/App.java
+++ 14_methods/src/App.java
@@ -1,3 +1,12 @@
+class Add {
+    int sum = 0;
+
+    public int addTwoNumbers(int a, int b){
+        sum = a + b;
+        return sum;
+    }
+}
+
 public class App {
 
     // method to print message
@@ -5,6 +14,10 @@ public class App {
         System.out.println("Hello worl!");
     }
     public static void main(String[] args) throws Exception {
+
+        Add a = new Add();
+        a.addTwoNumbers(10, 20);
+        System.out.println("Sum of two numbers is: " + a.sum);
         // print message function call 
         App obj = new App();
         obj.printMessage();
