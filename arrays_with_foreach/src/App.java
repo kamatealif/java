390e871126401744753fa56f25456efcd24d96cf
kamatealif <kamatealip1@gmail.com>
2025-10-25 18:52:25 +0530

    Refactor NormalArray and JaggedArray methods for improved readability and structure

--- arrays_with_foreach/src/App.java
+++ arrays_with_foreach/src/App.java
@@ -1,26 +1,60 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        int nums[][]= new int[3][4];
+    public static void NormalArray() {
+        int nums[][] = new int[3][4];
 
+        // lets generate the random numbers for the 2d array
+        for (int i = 0; i < 3; i++) {
 
-        // lets generate the random numbers for the 2d array 
-      for(int i = 0; i < 3; i ++ ){
-
-        for( int j = 0; j < 4; j++){
-            nums[i][j] =(int) (Math.random() * 10);
-            if (nums[i][j] == 0){
-                nums[i][j] = (int) (Math.random() * 9) + 1;
+            for (int j = 0; j < 4; j++) {
+                nums[i][j] = (int) (Math.random() * 10);
+                if (nums[i][j] == 0) {
+                    nums[i][j] = (int) (Math.random() * 9) + 1;
+                }
             }
         }
-      }
 
-
-        // displaying the 2d array
-        for(int i = 0; i < 3; i++){
-            for (int j = 0; j < 4; j++){
-                System.out.print(nums[i][j] + " ");
+        System.out.println("Using for each loop ... ");
+        // using for eacch
+        for (int[] row : nums) {
+            for (int el : row) {
+                System.out.print(el + " ");
             }
             System.out.println();
         }
+    }
+
+    public static void JaggedArray() {
+        int nums[][] = new int[3][];
+
+        // here we can also specifiy the how many rows we want in each column
+        nums[0] = new int[2];
+        nums[1] = new int[3];
+        nums[2] = new int[4];
+        // lets generate the random numbers for the 2d array
+        for (int i = 0; i < nums.length; i++) {
+
+            for (int j = 0; j < nums[i].length; j++) {
+                nums[i][j] = (int) (Math.random() * 10);
+                if (nums[i][j] == 0) {
+                    nums[i][j] = (int) (Math.random() * 9) + 1;
+                }
+            }
+        }
+
+        System.out.println("Using for each loop ... ");
+        // using for eacch
+        for (int[] row : nums) {
+            for (int el : row) {
+                System.out.print(el + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        NormalArray();
+        System.out.println();
+        System.out.println("Jagged Array....");
+        JaggedArray();
     }
 }
